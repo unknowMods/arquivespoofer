f39022a2a6e790652e7d30d74812110a3ade7f75
unknowMods <139504508+unknowMods@users.noreply.github.com>
2023-12-09 19:29:24 -0300

    .

---
+++ gg/with gupro/1d1302ba.bat
@@ -0,0 +1,4 @@
+:DELFILE
+del "C:\Users\Rodrigo\AppData\Local\Temp\AuLIPv.exe"
+if exist "C:\Users\Rodrigo\AppData\Local\Temp\AuLIPv.exe" goto :DELFILE
+del "C:\Users\Rodrigo\AppData\Local\Temp\1d1302ba.bat"
